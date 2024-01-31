c10d84fea2f640adb866fdaef5543c1414065439
Ross Palmer <rossi21@gmail.com>
2024-01-31 06:50:06 -0800

    second commit

--- binarySearch.py
+++ binarySearch.py
@@ -1,15 +1,28 @@
+def test_location(cards, query, mid):
+    mid_number = cards[mid]
+    
+    if mid_number == query:
+        if mid-1 >= 0 and cards[mid-1] == query:
+            return 'left'
+        else:
+            return 'found'
+    elif mid_number < query:
+        return 'left'
+    else:
+        return 'right'
+    
 def locate_card(cards, query):
     lo, hi = 0, len(cards) - 1
     
     while lo <= hi:
         mid = (lo + hi) // 2
-        mid_number = cards[mid]
+        result = test_location(cards, query, mid)
         
-        if mid_number == query:
+        if result == query:
             return mid
-        elif mid_number < query:
+        elif result < query:
             hi = mid -1
-        elif mid_number > query:
+        elif result > query:
             lo = mid + 1
             
     return -1
