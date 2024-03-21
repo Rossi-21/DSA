64dc3bceb9dc926877ff14fee38753a5a84b4911
Ross Palmer <rossi21@gmail.com>
2024-03-21 06:42:14 -0700

    pair_sum 2

--- pair_sum.py
+++ pair_sum.py
@@ -4,18 +4,19 @@ def pair_sum(numbers, target_sum):
     # iterate through the array
     while j < len(numbers)-1:
         j+=1
-        print(numbers[j], numbers[i])
+        print(numbers[i], numbers[j])
         sum = numbers[j] + numbers[i]
         print('sum = ' + str(sum))
         # check if numbers in the array sum to the target
         if sum  == target_sum:
             result = (i,j)
+        if j == len(numbers)-1:
+            i+=1
+            j=i
         else:
             print('nope')
-        
-    
     # retrun the indices in a tuple
     print(result)
     return result
 
-pair_sum([4, 7, 9, 2, 5, 1], 3) # -> (3, 5)
+pair_sum([1, 6, 7, 2], 13) 
