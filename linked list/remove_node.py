2ef3abe20ec03c41d6faf1ccd58181ba6c245113
Ross Palmer <rossi21@gmail.com>
2024-04-04 07:21:38 -0700

    insert node

--- linked list/remove_node.py
+++ linked list/remove_node.py
@@ -1,3 +1,4 @@
+# My Solution
 def remove_node(head, target_val):
     current = head
     previous = None
@@ -10,5 +11,20 @@ def remove_node(head, target_val):
                 previous.next = current.next
                 return head
         previous = current
+        current = current.next
+    return head
+
+# Iterative solution
+def remove_node(head, target_val):
+    if head.val == target_val:
+        return head.next
+    current = head
+    prev = None
+    
+    while current is not None:
+        if current.val == target_val:
+            prev.next = current.next
+            break
+        prev = current
         current = current.next
     return head
