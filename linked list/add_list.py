cd0668d0f05d6e9e865c76a357ea2b6156f56d90
Ross Palmer <rossi21@gmail.com>
2024-04-14 07:05:27 -0700

    add list 2

--- linked list/add_list.py
+++ linked list/add_list.py
@@ -1,5 +1,6 @@
+# recursive solution
 def add_lists(head_1, head_2, carry = 0):
-    if head_1 is None and head_2 is None:
+    if head_1 is None and head_2 is None and carry == 0:
         return None
     
     val_1 = 0 if head_1 is None else head_1.val
@@ -16,3 +17,28 @@ def add_lists(head_1, head_2, carry = 0):
     
     result_node.next = add_lists(next_1, next_2, carry)
     return result_node
+
+# Iterative Solution
+def add_lists(head_1, head_2):
+    dummy_head = Node(None)
+    tail = dummy_head
+    carry = 0
+    current_1 = head_1
+    current_2 = head_2
+    
+    while current_1 is not None or current_2 is not None or carry == 1:
+        val_1 = 0 if current_1 is None else current_1.val
+        val_2 = 0 if current_2 is None else current_2.val
+        
+        sum = val_1 + val_2 + carry
+        carry = 1 if sum > 9 else 0
+        digit = sum % 10
+        
+        tail.next = Node(digit)
+        tail = tail.next
+        
+        if current_1 is not None:
+        current_1 = current_1.next
+        if current_2 is not None:
+        current_2 = current_2.next
+    return dummy_head.next
